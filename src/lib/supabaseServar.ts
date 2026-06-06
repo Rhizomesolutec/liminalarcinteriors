b35c164d1e20fe9ac1c0fb0ca40bcf6ab1be9623
Rhizomesolutec <164726203+Rhizomesolutec@users.noreply.github.com>
2026-06-07 00:27:49 +0530

    Update supabaseServar.ts

--- src/lib/supabaseServar.ts
+++ src/lib/supabaseServar.ts
@@ -5,7 +5,8 @@ export async function createClient() {
   const cookieStore = await cookies()
 
   return createServerClient(
-    "https://vagytysnkicbwgavdcyq.supabase.co"
+    "https://vagytysnkicbwgavdcyq.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZhZ3l0eXNua2ljYndnYXZkY3lxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg1OTU1NzEsImV4cCI6MjA2NDE3MTU3MX0.sIXIz5DmCisX35m9O8F_8n8QiMb_5ANS0uV__XyIhkk"
     ,
     {
       cookies: {
